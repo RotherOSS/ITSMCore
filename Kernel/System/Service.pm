de1b6d190bff78f7f9722b81a8c3069c7aa2371c
Sven <svo@otobo.org>
2022-08-18 10:28:44 +0200

    Update to OTOBO 10.1.5; prepare for version 10.1.2;

--- Kernel/System/Service.pm
+++ Kernel/System/Service.pm
@@ -2,9 +2,9 @@
 # OTOBO is a web-based ticketing system for service organisations.
 # --
 # Copyright (C) 2001-2020 OTRS AG, https://otrs.com/
-# Copyright (C) 2019-2021 Rother OSS GmbH, https://otobo.de/
+# Copyright (C) 2019-2022 Rother OSS GmbH, https://otobo.de/
 # --
-# $origin: otobo - b1c1ab35d59104476ebde772a12c7f3cd36e5211 - Kernel/System/Service.pm
+# $origin: otobo - e894aef610208fdc401a4df814ca59658292fbba - Kernel/System/Service.pm
 # --
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
